eed85451b85a4197455941a4a76253432feea5d8
AbreezaSaleem <abreeza.saleem@hotmail.com>
2021-03-27 01:16:21 +0500

    modal added

--- src/components/Audio/Info.js
+++ src/components/Audio/Info.js
@@ -1,15 +1,37 @@
-import React, { useContext } from 'react';
+import React, { createRef, useContext, useEffect } from 'react';
+
 import * as styles from '@styles/modules/audio.module.scss';
+
+import SecondaryInfo from '@components/SecondaryInfo';
+
 import LocaleContext from '../../context/LocaleContext';
+
 import Translator from '../../content/Translator';
 
 const Info = () => {
+  const ref = createRef();
   const { locale } = useContext(LocaleContext);
+
+  useEffect(() => {
+    if (ref.current) {
+      const container = ref.current;
+      container.style.visibility = 'hidden';
+    }
+  }, []);
+
+  const openModal = () => {
+    if (ref.current) {
+      const container = ref.current;
+      container.style.visibility = 'visible';
+    }
+  };
 
   return (
     <div className={styles.infoContainer}>
+      <SecondaryInfo ref={ref} />
       <Translator type="heading" locale={locale} message="info.heading" />
       <Translator type="detail" locale={locale} message="info.detail" />
+      <button onClick={openModal} type="button">Learn more</button>
     </div>
   );
 };
