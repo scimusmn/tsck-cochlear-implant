8159b625d0ed24108c009268216d2936f96425a5
AbreezaSaleem <abreeza.saleem@hotmail.com>
2021-04-25 17:22:45 +0500

    unwanted files removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,13 @@ import App from '@components/App';
 import Home from '@components/Home';
 import { LocaleProvider } from '@context/LocaleContext';
 import { AudioProvider } from '@context/AudioContext';
-import { ImagesProvider } from '@context/ImagesContext';
-
-// TODO: maybe consider using this https://twitter.com/SimonHoiberg/status/1362389502753468416
 
 const IndexPage = () => (
   <LocaleProvider>
     <AudioProvider>
-      <ImagesProvider>
-        <App>
-          <Home />
-        </App>
-      </ImagesProvider>
+      <App>
+        <Home />
+      </App>
     </AudioProvider>
   </LocaleProvider>
 );
